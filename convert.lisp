69e94a95ba9ec321731364f93d578e54d97191bd
Max Craigie <max.craigie.ftw@gmail.com>
2011-09-30 18:27:10 +1000

    fixed up the licenses

--- convert.lisp
+++ convert.lisp
@@ -71,6 +71,35 @@
               (write-word i))
             (write-word i)))
       (write-sequence newline csv-stream))))
+	;; Copyright (c) 2002-2006, Edward Marco Baringer
+	;; All rights reserved. 
+	;; 
+	;; Redistribution and use in source and binary forms, with or without
+	;; modification, are permitted provided that the following conditions are
+	;; met:
+	;; 
+	;;  - Redistributions of source code must retain the above copyright
+	;;    notice, this list of conditions and the following disclaimer.
+	;; 
+	;;  - Redistributions in binary form must reproduce the above copyright
+	;;    notice, this list of conditions and the following disclaimer in the
+	;;    documentation and/or other materials provided with the distribution.
+	;;
+	;;  - Neither the name of Edward Marco Baringer, nor BESE, nor the names
+	;;    of its contributors may be used to endorse or promote products
+	;;    derived from this software without specific prior written permission.
+	;; 
+	;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+	;; "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+	;; LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+	;; A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT
+	;; OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+	;; SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+	;; LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+	;; DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+	;; THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	;; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+	;; OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 (defun replace-all (string part replacement &key (test #'char=))
 "Returns a new string in which all the occurences of the part 
@@ -86,6 +115,5 @@ is replaced with replacement."
                              :end (or pos (length string)))
             when pos do (write-string replacement out)
             while pos)))
-
-(defun setup ()
-  (load "/Users/maxcraigie/Desktop/codon.lisp"))
+;from http://cl-cookbook.sourceforge.net/strings.html
+;Copyright © 2002-2007 The Common Lisp Cookbook Project
